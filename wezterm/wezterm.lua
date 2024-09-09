4808cb8bed8d82634661ef28416712138e827c92
BrandenXia <xxtbranden@outlook.com>
2024-09-08 19:49:16 -0700

    Update wezterm config

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -160,19 +160,8 @@ wezterm.plugin.require("https://github.com/nekowinston/wezterm-bar").apply_to_co
 	max_width = 32,
 	dividers = "slant_right", -- or "slant_left", "arrows", "rounded", false
 	indicator = {
-		leader = {
-			enabled = true,
-			off = " ",
-			on = " ",
-		},
-		mode = {
-			enabled = true,
-			names = {
-				resize_mode = "RESIZE",
-				copy_mode = "VISUAL",
-				search_mode = "SEARCH",
-			},
-		},
+		leader = { enabled = false },
+		mode = { enabled = false },
 	},
 	tabs = {
 		numerals = "arabic", -- or "roman"
@@ -184,7 +173,7 @@ wezterm.plugin.require("https://github.com/nekowinston/wezterm-bar").apply_to_co
 	},
 	clock = { -- note that this overrides the whole set_right_status
 		enabled = true,
-		format = "%H:%M", -- use https://wezfurlong.org/wezterm/config/lua/wezterm.time/Time/format.html
+		format = "%H:%M ", -- use https://wezfurlong.org/wezterm/config/lua/wezterm.time/Time/format.html
 	},
 })
 
