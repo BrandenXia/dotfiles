f3adf93aee49e9729f106f3925394265eac00532
BrandenXia <xxtbranden@outlook.com>
2024-09-18 08:11:59 -0700

    Add common shell config and scripts

--- install.py
+++ install.py
@@ -23,12 +23,33 @@ class Helper:
     ):
         os.symlink(dotfile_dir / file_name, target_dir / file_name)
 
+    @staticmethod
+    def add_to_file(file_path: Path, content: str | list[str]):
+        with open(file_path, "a") as f:
+            f.write("\n")
+            content = content if isinstance(content, str) else "\n".join(content)
+            f.write(content)
+            f.write("\n")
+
+    @staticmethod
+    def add_to_zshrc(content: str | list[str]):
+        Helper.add_to_file(home_dir / ".zshrc", content)
+
 
 # edit to add more configs
 configs: dict[str, Callable[[str, Path], None]] = {
     "wezterm": Helper.link_to_config,
     "yazi": Helper.link_to_config,
     "nvim": Helper.link_to_config,
+    "zsh": lambda _, dotfile_dir: Helper.add_to_zshrc(
+        [
+            "# common config from dotfiles",
+            f"source {(dotfile_dir / 'common.zsh').absolute()}",
+        ],
+    ),
+    "scripts": lambda _, dotfile_dir: Helper.add_to_zshrc(
+        ["# scripts from dotfiles", f"export PATH=$PATH:{dotfile_dir.absolute()}"]
+    ),
 }
 
 
---
+++ zsh/common.zsh
@@ -0,0 +1,25 @@
+# setopt
+setopt extended_history
+
+# editor
+export EDITOR=nvim
+
+# alias
+alias vi=nvim
+alias vim=nvim
+alias ls="eza --icons"
+alias ll="ls-l"
+alias la="ls -la"
+alias lg=lazygit
+alias sys=fastfetch
+alias ra=yazi
+
+# yazi
+function r() {
+	local tmp="$(mktemp -t "yazi-cwd.XXXXXX")"
+	yazi "$@" --cwd-file="$tmp"
+	if cwd="$(cat -- "$tmp")" && [ -n "$cwd" ] && [ "$cwd" != "$PWD" ]; then
+		builtin cd -- "$cwd"
+	fi
+	rm -f -- "$tmp"
+}
