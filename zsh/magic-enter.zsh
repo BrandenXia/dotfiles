447a6de73863ca38319456a79177a7578c02c3e1
BrandenXia <xxtbranden@outlook.com>
2024-12-15 11:52:02 -0800

    Use same command for git directory and normal directory

--- zsh/magic-enter.zsh
+++ zsh/magic-enter.zsh
@@ -1,8 +1,7 @@
 # From https://github.com/ohmyzsh/ohmyzsh/blob/master/plugins/magic-enter/magic-enter.plugin.zsh
 
 # Default commands
-: ${MAGIC_ENTER_GIT_COMMAND:="git status -u ."} # run when in a git repository
-: ${MAGIC_ENTER_OTHER_COMMAND:="ls -lh ."}      # run anywhere else
+: ${MAGIC_ENTER_COMMAND:="ls -lh ."}      # run anywhere else
 
 magic-enter() {
   # Only run MAGIC_ENTER commands when in PS1 and command line is empty
@@ -11,11 +10,7 @@ magic-enter() {
     return
   fi
 
-  if command git rev-parse --is-inside-work-tree &>/dev/null; then
-    BUFFER="$MAGIC_ENTER_GIT_COMMAND"
-  else
-    BUFFER="$MAGIC_ENTER_OTHER_COMMAND"
-  fi
+  BUFFER="$MAGIC_ENTER_COMMAND"
 }
 
 # Wrapper for the accept-line zle widget (run when pressing Enter)
