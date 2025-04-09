fd5f725984f3c4afa2d9b0a23479e64fc4e9f1da
BrandenXia <xxtbranden@outlook.com>
2025-04-09 09:40:16 -0700

    feat(LazyVim): typst support

---
+++ nvim/lua/plugins/typst.lua
@@ -0,0 +1,32 @@
+return {
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = { ensure_installed = { "typst" } },
+  },
+
+  {
+    "neovim/nvim-lspconfig",
+    opts = {
+      servers = {
+        tinymist = {
+          root_dir = function(fname)
+            return require("lspconfig.util").root_pattern("*.typ")(fname)
+          end,
+        },
+      },
+    },
+  },
+
+  {
+    "chomosuke/typst-preview.nvim",
+    ft = "typst",
+    version = "1.*",
+    dependencies = { "williamboman/mason.nvim" },
+    opts = {
+      invert_colors = "auto",
+      dependencies_bin = {
+        ["tinymist"] = vim.fn.stdpath("data") .. "/mason/bin/tinymist",
+      },
+    },
+  },
+}
